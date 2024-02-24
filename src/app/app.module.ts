6b225a02f21d3dd465510eb0fbb1b58bb1cd6a0c
jerome <jerome.martinsmosca@gmail.com>
2024-02-24 16:08:39 -0500

    add files todo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RecipeFormComponent } from './components/recipe-form/recipe-form.component';
+import { IngredientFormComponent } from './components/ingredient-form/ingredient-form.component';
+import { RecipeDetailComponent } from './components/recipe-detail/recipe-detail.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { TagFormComponent } from './components/tag-form/tag-form.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeFormComponent,
+    IngredientFormComponent,
+    RecipeDetailComponent,
+    DashboardComponent,
+    TagFormComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
